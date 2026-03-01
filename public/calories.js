8c0af5ed7bfffe90ca9e23dcf8e785b6ce8bb94a
lebronjames9 <finerperson@gmail.com>
2026-03-01 16:57:57 -0500

    import fatsecret.js for API Calling to find calories of food items

---
+++ public/calories.js
@@ -0,0 +1,9 @@
+import FatSecret from 'fatsecret.js';
+
+const client = new FatSecret.Client({
+    credentials: {
+        clientId: '40472b4c09414602bd0780b358134319',
+        clientSecret: 'beb34fc589fc42318c41beef0d6359fd'
+    }
+})
+
