a1ebc85e209440911d4d74af7e52f83994dfffbb
lebronjames9 <finerperson@gmail.com>
2026-03-01 19:44:50 -0500

    added crawler to find images using Bing

--- public/calories.py
+++ public/calories.py
@@ -1,15 +1,15 @@
 import base64
 from fatsecret import Fatsecret
 from fuzzywuzzy import fuzz
-from icrawler.builtin import GoogleImageCrawler
+from icrawler.builtin import BingImageCrawler
 from icrawler.storage import BaseStorage
 
 #For authentication
 fs = Fatsecret('40472b4c09414602bd0780b358134319', 'a91b753f0a5441bbae1e9ed508885980')
 
 #Name of food and restaurant
-test_food = "chicken sandwich"
-test_restaurant = "chickfila"
+test_food = "big mac"
+test_restaurant = "mcdonalds"
 
 #Search database for food from restaurant
 search = test_restaurant + " " + test_food
@@ -34,4 +34,9 @@ food_info = fs.food_get(food_id)
 
 calories = food_info['servings']['serving']['calories']
 print("Calories: " + calories)
+
+crawler = BingImageCrawler(storage={'root_dir':'images/bing'})
+
+crawler.crawl(keyword=search, max_num=1)
+
 
