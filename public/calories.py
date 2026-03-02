b9b1da8127c82fd9804767202459653a2b7025d2
skullcruher65 <abrahamgeorgechackieth@gmail.com>
2026-03-01 23:50:47 -0500

    updated server side py

--- public/calories.py
+++ public/calories.py
@@ -1,42 +1,45 @@
-import base64
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from fatsecret import Fatsecret
 from fuzzywuzzy import fuzz
+from icrawler import ImageDownloader
 from icrawler.builtin import BingImageCrawler
-from icrawler.storage import BaseStorage
 
-#For authentication
+app = Flask(__name__)
+CORS(app)
+
 fs = Fatsecret('40472b4c09414602bd0780b358134319', 'a91b753f0a5441bbae1e9ed508885980')
 
-#Name of food and restaurant
-test_food = "big mac"
-test_restaurant = "mcdonalds"
+class UrlCollector(ImageDownloader):
+    def download(self, task, default_ext, req_timeout=None, **kwargs):
+        self.signal.urls.append(task['file_url'])
 
-#Search database for food from restaurant
-search = test_restaurant + " " + test_food
-foods = fs.foods_search(search)
+@app.route('/calories')
+def get_calories():
+    food = request.args.get('food')
+    restaurant = request.args.get('restaurant')
+    search = restaurant + " " + food
 
-#Get most similar food to user's food name from restaurant items
-food_result = ""
-similarity = 0
-for food in foods:
-    name = food.get('food_name')
+    # Get calories
+    foods = fs.foods_search(search)
+    food_result = None
+    similarity = 0
+    for f in foods:
+        if fuzz.partial_ratio(search, f) > similarity:
+            similarity = fuzz.partial_ratio(search, f)
+            food_result = f
 
-    if (fuzz.partial_ratio(search, food) > similarity):
-        similarity = fuzz.partial_ratio(search, food)
-        food_result = food
+    food_info = fs.food_get(food_result['food_id'])
+    calories = food_info['servings']['serving']['calories']
 
-print(food_result)
+    # Get image URL
+    crawler = BingImageCrawler(downloader_cls=UrlCollector)
+    crawler.downloader.signal.urls = []
+    image = crawler.crawl(keyword=search, max_num=1)
+    urls = crawler.downloader.signal.urls
+    image_url = urls[0] if urls else None
 
-food_id = food_result['food_id']
+    return jsonify({'calories': calories, 'image': image_url})
 
-#Info about the food (Nutrition, Serving size)
-food_info = fs.food_get(food_id)
-
-calories = food_info['servings']['serving']['calories']
-print("Calories: " + calories)
-
-crawler = BingImageCrawler(storage={'root_dir':'images/bing'})
-
-crawler.crawl(keyword=search, max_num=1)
-
-
+if __name__ == '__main__':
+    app.run(port=5000)
