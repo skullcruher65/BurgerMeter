cb4cd14081e5cc78acb9998f4ee2d0e876f81e9b
lebronjames9 <finerperson@gmail.com>
2026-03-01 19:02:59 -0500

    changes to calories.py for better database search

--- public/calories.py
+++ public/calories.py
@@ -1,17 +1,19 @@
+import base64
 from fatsecret import Fatsecret
 from fuzzywuzzy import fuzz
+from icrawler.builtin import GoogleImageCrawler
+from icrawler.storage import BaseStorage
 
 #For authentication
 fs = Fatsecret('40472b4c09414602bd0780b358134319', 'a91b753f0a5441bbae1e9ed508885980')
 
 #Name of food and restaurant
-test_food = "quesarito"
-test_restaurant = "taco bell"
+test_food = "chicken sandwich"
+test_restaurant = "chickfila"
 
-#Search database for restaurant
-foods = fs.foods_search(test_restaurant)
-
-print(foods)
+#Search database for food from restaurant
+search = test_restaurant + " " + test_food
+foods = fs.foods_search(search)
 
 #Get most similar food to user's food name from restaurant items
 food_result = ""
@@ -19,8 +21,8 @@ similarity = 0
 for food in foods:
     name = food.get('food_name')
 
-    if (fuzz.partial_ratio(test_food, food) > similarity):
-        similarity = fuzz.partial_ratio(test_food, food)
+    if (fuzz.partial_ratio(search, food) > similarity):
+        similarity = fuzz.partial_ratio(search, food)
         food_result = food
 
 print(food_result)
@@ -31,4 +33,5 @@ food_id = food_result['food_id']
 food_info = fs.food_get(food_id)
 
 calories = food_info['servings']['serving']['calories']
-print(calories)
+print("Calories: " + calories)
+
